e03bb16dcc7c33a1b87d8dd3ae537f06af6fc560
Brian Sunter <bsunter@gmail.com>
2017-05-16 22:51:52 -0700

    Basic Comments

--- src/hackernews/shared/scenes/detail_view/detail_view.cljs
+++ src/hackernews/shared/scenes/detail_view/detail_view.cljs
@@ -11,15 +11,19 @@
 
 (defn comment-row
   [comment]
-  [rn/view {:style {:padding 15}}
-   [rn/html-view {:value comment
+  [rn/view {:style {:margin 10}}
+   [rn/text {:style {:padding 5
+                     :color "#f26522"}}
+    (str (:user comment) " " (:time_ago comment))]
+   [rn/html-view {:value (:content comment)
+                  :style {:flex 1}
                   :stylesheet {:p {:font-size 18}}}]])
 
 (defn collect-comments
   [comment]
-  (->> comment
-       (tree-seq map? :comments)
-       (map :content)))
+  (->>
+   (tree-seq map? :comments comment)
+   (filter :content)))
 
 (defn detail-view
   [{:keys [id]}]
